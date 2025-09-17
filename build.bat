f8e9f8623f4721587f9e40a842001fc07eeea044
Eliott Takvorian <98952935+eliotttak@users.noreply.github.com>
2025-09-17 21:43:50 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,16 +4,6 @@ set GOOS=%1
 set GOARCH=%2
 set EXT=
 
-if "%GOOS%" == "" (
-    FOR /F "delims=" %%i IN ('go env GOOS') DO set GOOS=%%i
-)
-
-if "%GOARCH%" == "" (
-    FOR /F "delims=" %%i IN ('go env GOARCH') DO set GOARCH=%%i
-)
-
-
-
 if "%GOOS%" == "windows" (
     set EXT=.exe
 )
@@ -22,7 +12,7 @@ echo Building from .\src\ to .\bin\portables\GoFileEncoder_portable_%GOOS%_%GOAR
 
 mkdir .\bin\portables\
 
-rsrc -arch=%GOARCH% -ico icons\icon_16.ico,icons\icon_32.ico,icons\icon_64.ico,icons\icon_128.ico,icons\icon_256.ico -o .\pkg\rsrc.syso
+rsrc -arch=%GOARCH% -ico icons\icon_16.ico,icons\icon_32.ico,icons\icon_64.ico,icons\icon_128.ico,icons\icon_256.ico -o .\src\rsrc.syso
 
 go-bindata -pkg assets -o assets/bindata.go LICENSE
-go build -o .\bin\portables\GoFileEncoder_portable_%GOOS%_%GOARCH%%EXT% .\pkg\
+go build -o .\bin\portables\GoFileEncoder_portable_%GOOS%_%GOARCH%%EXT% .\src\
